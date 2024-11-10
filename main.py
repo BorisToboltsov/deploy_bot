8428541595483e600212bb3083acf73ecb8d33e4
BorisToboltsov <b.toboltsov@vcore.su>
2024-11-10 15:19:37 +0300

    add git test

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import git
 
 repo = git.Repo('/Users/boristoboltsov/project/deploy_bot', search_parent_directories=True)
 
-repo.remote('origin').pull()
+# Git pull
+# repo.remote('origin').pull()
 
-repo.git.checkout('telegram')
+try:
+    repo.git.checkout('telegram')
+except git.exc.GitError as e:
+    print(e)
 
+
+# Сброс всех изменений
 # repo.git.reset('--hard')
