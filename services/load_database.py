256b3167a531f3548484041ac52e81eccb658765
BorisToboltsov <b.toboltsov@vcore.su>
2024-12-01 21:50:56 +0300

    test

--- services/load_database.py
+++ services/load_database.py
@@ -12,12 +12,12 @@ from bot.view.load_database import load_database_complete_view
 async def load_database(db_name: str, telegram_id: int, state: FSMContext) -> NoReturn:
     reset_master = f'mysql -u {os.getenv("USER_BACKUP")} -p{os.getenv("PASSWORD_BACKUP")} -Bse "RESET MASTER;"'
     # os.system(reset_master)
-    subprocess.Popen(reset_master)
+    subprocess.Popen(reset_master.split(' '))
 
     current_date = datetime.datetime.now().strftime('%Y-%m-%d')
     load = f'mysql -u {os.getenv("USER_BACKUP")} -p{os.getenv("PASSWORD_BACKUP")} {db_name} < {os.getenv("PATH_TO_BACKUP")}/{db_name}_{current_date}.sql'
     # os.system(load)
-    subprocess.Popen(load)
+    subprocess.Popen(load.split(' '))
 
     await load_database_complete_view(telegram_id)
     await state.update_data(load_active=False)
