5cc53107496aafba9bba6a59c80008ca96b6d722
BorisToboltsov <b.toboltsov@vcore.su>
2024-12-01 21:48:40 +0300

    fix bug

--- services/load_database.py
+++ services/load_database.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import datetime
+import subprocess
 from typing import NoReturn
 from aiogram.fsm.context import FSMContext
 
@@ -10,11 +11,13 @@ from bot.view.load_database import load_database_complete_view
 
 async def load_database(db_name: str, telegram_id: int, state: FSMContext) -> NoReturn:
     reset_master = f'mysql -u {os.getenv("USER_BACKUP")} -p{os.getenv("PASSWORD_BACKUP")} -Bse "RESET MASTER;"'
-    os.system(reset_master)
+    # os.system(reset_master)
+    subprocess.Popen(reset_master)
 
     current_date = datetime.datetime.now().strftime('%Y-%m-%d')
     load = f'mysql -u {os.getenv("USER_BACKUP")} -p{os.getenv("PASSWORD_BACKUP")} {db_name} < {os.getenv("PATH_TO_BACKUP")}/{db_name}_{current_date}.sql'
-    os.system(load)
+    # os.system(load)
+    subprocess.Popen(load)
 
     await load_database_complete_view(telegram_id)
     await state.update_data(load_active=False)
