6c225193721f2979380a77f3e8897a0ee834012a
BorisToboltsov <b.toboltsov@vcore.su>
2024-12-01 22:51:14 +0300

    test

--- services/load_database.py
+++ services/load_database.py
@@ -18,6 +18,6 @@ async def load_database(db_name: str, telegram_id: int, state: FSMContext) -> No
     load = f'mysql -u {os.getenv("USER_BACKUP")} -p{os.getenv("PASSWORD_BACKUP")} {db_name} < {os.getenv("PATH_TO_BACKUP")}/{db_name}_{current_date}.sql'
     # os.system(load)
     t2 = subprocess.run(["mysql", "-u", os.getenv("USER_BACKUP"), f"-p{os.getenv("PASSWORD_BACKUP")}", db_name, "<", f"{os.getenv("PATH_TO_BACKUP")}/{db_name}_{current_date}.sql"], stdout=subprocess.PIPE)
-    await load_database_stdout_view(telegram_id, t2.stdout.decode('utf-8')[5000:6500])
+    await load_database_stdout_view(telegram_id, t2.stdout.decode('utf-8')[6500:9000])
     await load_database_complete_view(telegram_id)
     await state.update_data(load_active=False)
