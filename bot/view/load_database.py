2113687d0a0948c8afa40c9207361cee0fa4b810
BorisToboltsov <b.toboltsov@vcore.su>
2024-12-01 22:22:57 +0300

    test

--- bot/view/load_database.py
+++ bot/view/load_database.py
@@ -32,7 +32,7 @@ async def load_database_error_view(telegram_id: int) -> NoReturn:
 
 
 async def load_database_stdout_view(telegram_id: int, t1, t2) -> NoReturn:
-    context = f"{t1}\n{t2}"
+    context = f"a\n{t1}\n{t2}"
     await EntityMessage.send_message_from_user(
         telegram_id, context
     )
