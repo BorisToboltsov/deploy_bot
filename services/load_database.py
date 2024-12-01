d67197d9c7f769f6a46c5ec475ee3243e406a01b
BorisToboltsov <b.toboltsov@vcore.su>
2024-12-01 22:41:08 +0300

    test

--- services/load_database.py
+++ services/load_database.py
@@ -13,11 +13,11 @@ async def load_database(db_name: str, telegram_id: int, state: FSMContext) -> No
     reset_master = f'mysql -u {os.getenv("USER_BACKUP")} -p{os.getenv("PASSWORD_BACKUP")} -Bse "RESET MASTER;"'
     # os.system(reset_master)
     t1 = subprocess.run(["mysql", "-u", os.getenv("USER_BACKUP"), f"-p{os.getenv("PASSWORD_BACKUP")}", "-Bse", "RESET MASTER;"], stdout=subprocess.PIPE)
-    await load_database_stdout_view(telegram_id, t1.stdout.decode('utf-8'))
 
     current_date = datetime.datetime.now().strftime('%Y-%m-%d')
     load = f'mysql -u {os.getenv("USER_BACKUP")} -p{os.getenv("PASSWORD_BACKUP")} {db_name} < {os.getenv("PATH_TO_BACKUP")}/{db_name}_{current_date}.sql'
     # os.system(load)
     t2 = subprocess.run(["mysql", "-u", os.getenv("USER_BACKUP"), f"-p{os.getenv("PASSWORD_BACKUP")}", db_name, "<", f"{os.getenv("PATH_TO_BACKUP")}/{db_name}_{current_date}.sql"], stdout=subprocess.PIPE)
+    await load_database_stdout_view(telegram_id, t2.stdout.decode('utf-8'))
     await load_database_complete_view(telegram_id)
     await state.update_data(load_active=False)
