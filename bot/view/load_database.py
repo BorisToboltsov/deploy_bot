5c8f8027650ce585ee36158cd7985bc5c8dfc0e8
BorisToboltsov <b.toboltsov@vcore.su>
2024-12-01 03:26:42 +0300

    refactoring

--- bot/view/load_database.py
+++ bot/view/load_database.py
@@ -4,7 +4,7 @@ from bot.utils.send_message import EntityMessage
 
 
 async def load_database_start_view(telegram_id: int) -> NoReturn:
-    context = "Запуск загрузки базы данных, база данных загружается около 5 минут."
+    context = "Запуск загрузки базы данных, время выполнения около 5 минут."
     await EntityMessage.send_message_from_user(
         telegram_id, context
     )
