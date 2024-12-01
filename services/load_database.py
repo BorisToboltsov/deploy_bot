0fb3c5608da13840bff7bb63ce139bf079617613
BorisToboltsov <b.toboltsov@vcore.su>
2024-12-01 23:02:22 +0300

    test

--- services/load_database.py
+++ services/load_database.py
@@ -15,9 +15,21 @@ async def load_database(db_name: str, telegram_id: int, state: FSMContext) -> No
     t1 = subprocess.run(["mysql", "-u", os.getenv("USER_BACKUP"), f"-p{os.getenv("PASSWORD_BACKUP")}", "-Bse", "RESET MASTER;"], stdout=subprocess.PIPE)
 
     current_date = datetime.datetime.now().strftime('%Y-%m-%d')
+
+    command = [
+        'mysql',
+        '-u', os.getenv("USER_BACKUP"),
+        '-p' + os.getenv("PASSWORD_BACKUP"),
+        db_name
+        ]
     load = f'mysql -u {os.getenv("USER_BACKUP")} -p{os.getenv("PASSWORD_BACKUP")} {db_name} < {os.getenv("PATH_TO_BACKUP")}/{db_name}_{current_date}.sql'
     # os.system(load)
-    t2 = subprocess.run(["mysql", "-u", os.getenv("USER_BACKUP"), f"-p{os.getenv("PASSWORD_BACKUP")}", db_name, "<", f"{os.getenv("PATH_TO_BACKUP")}/{db_name}_{current_date}.sql"], stdout=subprocess.PIPE)
-    await load_database_stdout_view(telegram_id, t2.stdout.decode('utf-8')[:2500])
+    # t2 = subprocess.run(["mysql", "-u", os.getenv("USER_BACKUP"), f"-p{os.getenv("PASSWORD_BACKUP")}", db_name, "<", f"{os.getenv("PATH_TO_BACKUP")}/{db_name}_{current_date}.sql"], stdout=subprocess.PIPE)
+    # await load_database_stdout_view(telegram_id, t2.stdout.decode('utf-8')[:2500])
+    with open(f'{os.getenv("PATH_TO_BACKUP")}/{db_name}_{current_date}.sql', 'r') as file:
+        process = subprocess.run(
+        command,
+        stdin = file,
+        check = True           )
     await load_database_complete_view(telegram_id)
     await state.update_data(load_active=False)
